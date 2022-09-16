6ce7c229351a29a38941fde4debc6bcdb42ae448
RstTiwari <iamrrt2121@gmail.com>
2022-09-16 09:40:19 +0530

    method study

--- chapter6.js
+++ chapter6.js
@@ -1,4 +1,38 @@
 // New Chapter of the Book to learn Even more In the Deep values
 
 //Encapsulation:seprating the  inner interface  from the implementation is called as Encapsulations;
-//oo
+// Methods  : Method are basically Predeifned Function which Perform the Task on given Value
+
+let Rabbit = {}
+Rabbit.speak = function(line){
+     return (`Rabbit say ${line}`)
+}
+//console.log(Rabbit.speak("hey guys"))
+
+
+// it's better way of Defining Methods and parameter
+function speak(line){
+    return (`the  ${this.type} Raabit says ${line}`)
+}
+
+// this extra method(paramewter) pass in object , we can this it in diffrent way
+let whiteRabbit = {type : "White"  ,speak}
+let hungarryRabit = {type : "hungary" ,speak}
+
+whiteRabbit.speak("ohh the Man u are awsome ")
+hungarryRabit.speak("i can use the carrat right now")  
+
+// we can use  call method for explicitly using this method
+console.log(speak.call(hungarryRabit , "Burpppppp"))
+
+// Defining Method Using the array function
+
+const action = (line)=>{
+    return (`the action ${this.type} , take you thorugh ${line}`)
+}
+
+let running = {type:"Running" , action}
+//console.log(running.action("healthy life"))
+
+//====>>> PROTOTYPE
+//prodtotype : prototypes are another object
